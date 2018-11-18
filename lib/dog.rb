0b8ada8c7aeda84a32bb2b35c041e4150429d578
Akili Humphrey <akilihumphrey@gmail.com>
2018-11-18 20:19:32 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+ 
+  @@all = []
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    @@all = []
+end
+
+def Dog.all
+    puts @@all.map{ |dog| dog.name }
+  end
+end
