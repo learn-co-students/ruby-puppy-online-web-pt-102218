83c4eaee83d14016c92fbdc2d2155a2cdca1e3d9
Anysia V <anysiavalko@gmail.com>
2018-12-02 19:38:32 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,30 @@
+require 'pry'
+
+class Dog
+  
+  
+  @@all = [ ]
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all.each do |dog|
+      puts dog.name 
+    end
+  end 
+  
+  def self.clear_all
+    @@all = [ ]
+  end
+ 
+end
+    
+    
+#def self.all
+      #puts @@all.collect {|dog| dog.name}
+    #end
